5c202690269895fc61b1aba0b37cbffddf64657d
Shunting Zhang <shunting@fb.com>
2025-11-18 14:50:12 -0800

    [inductor] make mix order reduction work with dynamic shapes
    ghstack-source-id: 62f2d99abed0befa83afa3ca4f7dec63e1e038e2
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/168117

--- torch/_inductor/codegen/simd.py
+++ torch/_inductor/codegen/simd.py
@@ -1610,6 +1610,9 @@ class SIMDScheduling(BaseScheduling):
     def _codegen_mix_order_reduction(self, node1, node2):
         numel, rnumel = scheduler.MixOrderReduction.get_numel_rnumel(node1)
 
+        # the statically_known_gt works as expected for dynamic shapes
+        # since we already add guards in MixOrderReduction.can_fuse
+        # for dynamic shapes.
         if not V.graph.sizevars.statically_known_gt(
             numel,
             rnumel,
@@ -1625,7 +1628,10 @@ class SIMDScheduling(BaseScheduling):
             device_prop = DeviceProperties.create(node1.get_device())
             num_sm = device_prop.multi_processor_count
             estimated_num_splits = num_sm * 8
-            split_size = max(next_power_of_2(numel // estimated_num_splits), 16)
+
+            # split_size is decided based on hint
+            numel_hint = V.graph.sizevars.size_hint(numel)
+            split_size = max(next_power_of_2(numel_hint // estimated_num_splits), 16)
             split_size = min(split_size, 128)
             return split_size
 
@@ -1733,7 +1739,7 @@ class SIMDScheduling(BaseScheduling):
 
         # a extra round of reduction
         assert len(converted_nodes) == len(kernel.saved_partial_accumulate)
-        nsplit = (numel + split_size - 1) // split_size
+        nsplit = V.graph.wrapper_code.codegen_python_sizevar((numel + split_size - 1) // split_size)
         for idx, partial_accum in enumerate(kernel.saved_partial_accumulate):
             buffer_name = partial_accum.buffer_name
 
